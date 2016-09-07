5d77ae5df3a60b6d2ca9e5862802da90a55b1ae3
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-09-07 09:52:15 -0400

    Initial configuration for logging. Updating .gitignore and project dependencies.

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,5 +1,38 @@
 const config = require('./env');
 const mongoose = require('mongoose');
+const bunyan = require('bunyan');
+
+let serializer = data => {
+    let query = JSON.stringify(data.query);
+    let options = JSON.stringify(data.options || {});
+
+    return `db.${data.coll}.${data.method}(${query}, ${options});`;
+};
+
+let logger = bunyan.createLogger({
+    name: 'StylePoint',
+    src: false,
+    streams: [{path: `${__dirname}/db.log`}],
+    serializers: {
+        dbQuery: serializer
+    }
+});
+
+mongoose.set('debug', (coll, method, query, doc, options) => {
+    let set = {
+        coll: coll,
+        method: method,
+        query: query,
+        doc: doc,
+        options: options
+    };
+
+    logger.info({
+        dbQuery: set
+    });
+});
+
+mongoose.connection.on('error', err => logger.error(err));
 
 module.exports = () => {
     require('../models/user.server.model');
