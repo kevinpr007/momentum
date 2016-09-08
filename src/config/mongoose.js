6478e0eb302014f79cf90568bad557562549cb90
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-09-07 21:46:38 -0400

    Code refactoring logging feature. All log config is on config.logger.js. Adding bunyan rolling file stream to rotate log files. Updating .gitignore and dependencies.

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,24 +1,8 @@
 const config = require('./env');
 const mongoose = require('mongoose');
-const bunyan = require('bunyan');
+const logger = require('./logger');
 
-let serializer = data => {
-    let query = JSON.stringify(data.query);
-    let options = JSON.stringify(data.options || {});
-
-    return `db.${data.coll}.${data.method}(${query}, ${options});`;
-};
-
-let logger = bunyan.createLogger({
-    name: 'StylePoint',
-    src: false,
-    streams: [{path: '../logs/db.log'}],
-    serializers: {
-        dbQuery: serializer
-    }
-});
-
-mongoose.set('info', (coll, method, query, doc, options) => {
+mongoose.set('debug', (coll, method, query, doc, options) => {
     let set = {
         coll: coll,
         method: method,
@@ -32,7 +16,7 @@ mongoose.set('info', (coll, method, query, doc, options) => {
     });
 });
 
-mongoose.connection.on('error', err => logger.fatal(err));
+mongoose.connection.on('error', err => logger.error(err));
 
 module.exports = () => {
     require('../models/user.server.model');
@@ -41,7 +25,5 @@ module.exports = () => {
     require('../models/location.server.model');
     require('../models/service.server.model');
 
-    return {
-        connect: mongoose.connect(config('DB_URL'))
-    };
+    return mongoose.connect(config('DB_URL'));
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('./config/mongoose')();
 const userService = require('./services/userService')();
 
-console.log(userService.getAll());
+console.log(userService.getAll()
+    .then(console.log('Hello world!')).catch(err => console.log(err)));
