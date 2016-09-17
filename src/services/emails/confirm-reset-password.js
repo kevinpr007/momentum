281440b60f6e0e4145db0323d0e4a81d08c3558f
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-09-16 21:27:17 -0400

    Reset Password Email template Updating arguments.

--- src/services/emails/confirm-reset-password.js
+++ src/services/emails/confirm-reset-password.js
@@ -2,7 +2,7 @@ const Promise = require('bluebird')
 const env = require('../../config/env')
 const transporter = require('../../config/nodemailer')
 
-module.exports = (user, host, resetToken) => {
+module.exports = (user, host) => {
   let template = {
     from: env('EMAIL'),
     to: user.email,
@@ -11,7 +11,7 @@ module.exports = (user, host, resetToken) => {
         <p>
             You are receiving this because you (or someone else) have requested the reset of the password for your account.
             <br>Please click on the following link, or paste this into your browser to complete the process:<br><br>
-            http://${host}/reset-password/${resetToken}<br><br>
+            http://${host}/reset-password/${user.resetPasswordToken}<br><br>
             If you did not request this, please ignore this email and your password will remain unchanged.
         </p>
     `
