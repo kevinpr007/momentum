82c8955880319fe8bfdd40a77af5f13bbb86ff35
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-09-16 20:27:39 -0400

    Email templates Updating template names and parameters.

---
+++ src/services/emails/reset-password-confirm.js
@@ -0,0 +1,29 @@
+const Promise = require('bluebird')
+const env = require('../../config/env')
+const transporter = require('../../config/nodemailer')
+
+module.exports = (user, host, resetToken) => {
+  let template = {
+    from: env('EMAIL'),
+    to: user.email,
+    subject: 'Style Point: Reset your password',
+    html: `
+        <p>
+            You are receiving this because you (or someone else) have requested the reset of the password for your account.
+            <br>Please click on the following link, or paste this into your browser to complete the process:<br><br>
+            http://${host}/reset-password/${resetToken}<br><br>
+            If you did not request this, please ignore this email and your password will remain unchanged.
+        </p>
+    `
+  }
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(template, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
--- src/services/emails/reset-password.js
+++ src/services/emails/reset-password.js
@@ -2,17 +2,16 @@ const Promise = require('bluebird')
 const env = require('../../config/env')
 const transporter = require('../../config/nodemailer')
 
-module.exports = (req, resetToken) => {
+module.exports = user => {
   let template = {
     from: env('EMAIL'),
-    to: req.user.email,
-    subject: 'Style Point: Reset your password',
+    to: user.email,
+    subject: 'Style Point: Password Changed',
     html: `
         <p>
-            You are receiving this because you (or someone else) have requested the reset of the password for your account.
-            <br>Please click on the following link, or paste this into your browser to complete the process:<br><br>
-            http://${req.headers.host}/reset-password/${resetToken}<br><br>
-            If you did not request this, please ignore this email and your password will remain unchanged.
+            You are receiving this email because you changed your password.
+            <br>
+            If you did not request this change, please contact us immediately.          
         </p>
     `
   }
