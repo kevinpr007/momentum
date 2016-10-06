b632ab7c256b220895cb56df47b67c1f1ef66c28
jm5256 <jm5256@medicalcardsystem.com>
2016-10-06 11:43:11 -0400

    fixing benchmark/homepage.js

--- test/benchmark/homepage.js
+++ test/benchmark/homepage.js
@@ -1,7 +1,7 @@
 const siege = require('siege')
 
-// siege()
-//   .on(8080)
-//   .for(100).times
-//   .get('/')
-//   .attack();
+siege()
+  .on(8080)
+  .for(100).times
+  .get('/')
+  .attack();
