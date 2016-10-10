599ba20734991b31ebd44a44d2fde0c8d0497a3a
Kevin Rivera <kevinpr_007@hotmail.com>
2016-10-09 22:17:19 -0400

    Add the mongoose seed configuration and the User entity

---
+++ src/config/mongoose_seed/configuration.js
@@ -0,0 +1,22 @@
+const seeder = require('mongoose-seed')
+const config = require('../config')
+
+const mongoDB = require('../../config/mongoose.collections.json')
+const userData = require('../mongoose_seed/user.seed')
+
+// Connect to MongoDB via Mongoose
+seeder.connect(config.DB_URL, () => {
+
+    // Load Mongoose models
+    seeder.loadModels([
+        './src/models/user.server.model'
+    ]);
+
+    // Clear specified collections
+    seeder.clearModels([mongoDB.Model.User], () => {
+
+        // Callback to populate DB once collections have been cleared
+        seeder.populateModels(userData);
+
+    });
+});
