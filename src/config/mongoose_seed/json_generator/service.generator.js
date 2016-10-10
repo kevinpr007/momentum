ea38eb0cef7f23b343f928fc0bd9e8837a337376
Kevin Rivera <kevinpr_007@hotmail.com>
2016-10-09 23:19:13 -0400

    Add the seed service information

---
+++ src/config/mongoose_seed/json_generator/service.generator.js
@@ -0,0 +1,11 @@
+[
+    '{{repeat(100, 100)}}',
+    {
+        name: '{{lorem(1, "words")}}',
+        description: '{{lorem(1, "paragraphs")}}',
+        price: '{{floating(10, 60, 2, "0.00")}}',
+        time: '{{integer(20, 60)}}',
+        user: '{{objectId()}}',
+        createdBy: '{{objectId()}}'
+    }
+]
