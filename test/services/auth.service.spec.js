9d68ddbaa06621ad63661ac73e20f5d9d62e1630
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-10-09 21:59:05 -0400

    Auth service tests Updating reset-token test (passed). Adding reset-password test (failed).

--- test/services/auth.service.spec.js
+++ test/services/auth.service.spec.js
@@ -1,13 +1,50 @@
 const config = require('../../src/config/config')
-const MongooseError = require('mongoose/lib/error')
-
 const mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
-const User = require('../../src/models/user.server.model.js')
-let authService = require('../../src/services/auth.service.js')()
 
-describe('User authentication', () => {
+describe('User authentication service', () => {
+
+  const User = require('../../src/models/user.server.model.js')
+  let authService = require('../../src/services/auth.service.js')()
+
   describe('Given a user requesting to reset password with a valid email', () => {
-    it('will create a new reset-password token')
+    let mock = sinon.mock(new User({
+      email: 'test@dev.com',
+      password: 'Qwerty123'
+    }))
+
+    let user = mock.object
+    mock.expects('save').resolves(user)
+
+    it('will create a new reset-password token', done => {
+      authService.resetToken(user).then(user => {
+        assert.notEqual(user.resetPasswordToken, null)
+        done()
+      })
+    })
+  })
+
+  describe('Given a user resetting his/her password by providing a valid reset password token', () => {
+    let token = 'A1244'
+    let mock = sinon.mock(new User({
+      email: 'test@dev.com',
+      password: 'Qwerty123',
+      resetPasswordToken: token,
+      resetPasswordExpires: new Date().getDate() + 1
+    }))
+
+    mock.expects('findOne').withArgs({
+      resetPasswordToken: token,
+      resetPasswordExpires: {
+        $gt: new Date()
+      }
+    }).chain('exec').resolves(user)
+
+    it.only('will return a user by specified token', done => {
+      authService.resetUserPassword(token).then(user => {
+        console.error(user)
+        done()
+      })
+    })
   })
 })
