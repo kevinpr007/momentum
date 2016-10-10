f50fdc7e12c40f1de4953cc711562714b5d040b2
Kevin Rivera <kevinpr_007@hotmail.com>
2016-10-09 23:36:10 -0400

    Add the schedule seed

---
+++ src/config/mongoose_seed/json_generator/schedule.generator.js
@@ -0,0 +1,11 @@
+[
+    '{{repeat(100, 100)}}',
+    {
+        startDate: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}',
+        endDate: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}',
+        scheduleType: '{{random("General", "Exception")}}',
+        service: '{{objectId()}}',
+        workshift: '{{objectId()}}',
+        createdBy: '{{objectId()}}'
+    }
+]
