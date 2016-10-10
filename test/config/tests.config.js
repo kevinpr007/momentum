4073ff3c09dc6cbecc928aec3ea20561893e4064
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-10-09 21:57:17 -0400

    Tests config update Adding the following dev dependencies:     sinon mongoose     sinon-as-promised

--- test/config/tests.config.js
+++ test/config/tests.config.js
@@ -2,6 +2,9 @@ const chai = require('chai')
 chai.use(require('chai-as-promised'))
 
 global.expect = chai.expect
-global.should = chai.should
+global.should = chai.should()
 global.assert = chai.assert
+
 global.sinon = require('sinon')
+require('sinon-mongoose')
+require('sinon-as-promised')
