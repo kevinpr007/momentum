b9434c0f31d49f941edf8eaf79b732a90a8856ad
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-10-11 21:37:29 -0400

    User service tests
    Adding new tests for getById, getByEmail and getAll functions. All tests passed.

--- test/services/user.service.spec.js
+++ test/services/user.service.spec.js
@@ -7,15 +7,67 @@ describe('User service tests', () => {
   let userService = require('../../src/services/user.service.js')()
 
   describe('Given an email of an existing user', () => {
-    it('will return user by specified email')
+    it('will return user by specified email', sinon.test(function (done) {
+      let mock = this.mock(User)
+      let user = new User({
+        email: 'ivan@dev.com'
+      })
+
+      mock.expects('findOne').chain('where')
+          .withArgs('email', user.email)
+          .chain('exec').resolves(user)
+
+      userService.getByEmail('ivan@dev.com').then(user => {
+        mock.restore()
+        mock.verify()
+        assert.notEqual(user, null)
+        expect(user).to.have.property('_id')
+        done()
+      }).catch(err => done(err))
+    }))
   })
 
   describe('Given an Id of an existing user', () => {
-    it('will return user by specified Id')
+    it('will return user by specified Id', sinon.test(function (done) {
+      let _id = '1234-abcd'
+      let mock = this.mock(User)
+      let user = new User({
+        _id: _id,
+        email: 'test@dev.com'
+      })
+
+      mock.expects('findOne').chain('where')
+          .withArgs('_id', _id)
+          .chain('exec').resolves(user)
+
+      userService.getById(_id).then(user => {
+        mock.restore()
+        mock.verify()
+        assert.notEqual(user, null)
+        done()
+      }).catch(err => done(err))
+    }))
   })
 
   describe('Given a request to retrieve all existing users', () => {
-    it('will return all existing users')
+    it('will return all existing users', sinon.test(function (done) {
+      let mock = this.mock(User)
+      let users = [
+        new User({
+          email: 'test1@dev.com'
+        }), new User({
+          email: 'test2@dev.com'
+        })]
+
+      mock.expects('find').chain('exec').resolves(users)
+
+      userService.getAll().then(users => {
+        mock.restore()
+        mock.verify()
+        expect(users.length).to.equal(2)
+        done()
+      }).catch(err => done(err))
+    }))
   })
 
   describe('Given a request to register a new user', () => {
