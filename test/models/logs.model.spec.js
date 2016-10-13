06ba5cbcf91501db5c6adb9c526ae221be40890c
Kevin Rivera <kevinpr_007@hotmail.com>
2016-10-13 16:20:27 -0400

    Complete the custom logic in Log Model

---
+++ test/models/logs.model.spec.js
@@ -0,0 +1,16 @@
+describe('Logs schema validation tests', () => {
+  describe('Given a Logs model', () => {
+    let LogModel = require('../../src/models/logs.server.model.js')
+
+    it.only('Using the getError() function should return CODE - STATUS - MESSAGE', (done) => {
+      let log = new LogModel({
+        code: "400",
+        status: "error",
+        message: "Not Found"
+      })
+
+      expect(log.getError).to.be.equal('400 - error - Not Found')
+      done()
+    })
+  })
+})
