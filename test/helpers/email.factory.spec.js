3a01e9a6641315a793f058d37f9f427836232791
Kevin Rivera <kevinpr_007@hotmail.com>
2016-10-19 13:59:26 -0400

    Complete the email factory test

---
+++ test/helpers/email.factory.spec.js
@@ -0,0 +1,21 @@
+const config = require('../../src/config/config')
+
+describe('Email factory tests', () => {
+  describe('Given a base information', () => {
+
+    const to = 'from@stylepoint.com'
+    const subject = 'Test subject'
+    const html = '<p>Template</p>'
+    const factory = require('../../src/helpers/email.factory')
+
+    it('will generate an email template', sinon.test(function (done) {
+      let emailInfo = factory(to, subject, html).getInfo()
+
+      assert.notEqual(emailInfo, null)
+      expect(emailInfo.to).to.be.equal(to)
+      expect(emailInfo.subject).to.be.equal(subject)
+      expect(emailInfo.html).to.be.equal(html)
+      done()
+    }))
+  })
+})
