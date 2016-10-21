fdef20048505966a225ed95cb5e4c0a8e5d04236
Kevin Rivera <kevinpr_007@hotmail.com>
2016-10-21 14:51:45 -0400

    Complete the Log Get Success

--- test/controllers/log.controller.spec.js
+++ test/controllers/log.controller.spec.js
@@ -1,84 +1,55 @@
-const logService = require('../../src/services/log.service')
-let Log = require('../../src/models/logs.server.model')
-var httpMocks = require('node-mocks-http');
-var request22 = require('dupertest')
-
-
-describe('Log controller test', () => {
-
-// /logs
 // /logs/code/:code
 // /logs/status/:status
 
+const logService = require('../../src/services/log.service')
+const Log = require('../../src/models/logs.server.model')
+
+var httpMocks = require('node-mocks-http')
+
+describe('Log controller test', () => {
+
+  // /logs Success
   describe('Given a request to get all logs in the system', () => {
-    it.only('returns an array of all logs in Database', sinon.test(function (done) {
-        
-        let logArr = [new Log({code: '200'}), new Log({code: '201'})]
-        let resultTest = Promise.resolve(logArr)
-        
-        const namespace = {
-          Service: require('../../src/services/log.service')
-        };
-      
-        let object = {
-          getAll() {
-            return resultTest
-          }
+    it('returns an array of all logs in Database', sinon.test(function (done) {
+      let req = httpMocks.createRequest()
+      let res = httpMocks.createResponse({eventEmitter: require('events').EventEmitter})
+
+      let logArray = [new Log({code: '200'}), new Log({code: '201'})]
+      let PromiseTest = Promise.resolve(logArray)
+
+      const namespace = {
+        MockService: require('../../src/services/log.service')
+      }
+
+      let methods = {
+        getAll() {
+          return PromiseTest
         }
+      }
 
-        var req = httpMocks.createRequest()
-        var res = httpMocks.createResponse({eventEmitter: require('events').EventEmitter})
+      let next = (err) => {
+        done(err)
+      }
 
-        sinon.stub(namespace, 'Service').returns(object);
-        const logController = require('../../src/controllers/log.controller')(namespace.Service())
-        
-        function next(arg){
-          
-          done(arg)
-          
-          // expect(1).to.equal(30)
-          // expect.verify()
-          // done()
-          // return new Error("asdasdasd")
-          
-          // try {
-          //   // expect(args).to.be.a('Error')
-          //   assert.notEqual(1, 1)
-          // } catch (err) {
-          //   done(err) 
-          // }
-         }
+      sinon.stub(namespace, 'MockService').returns(methods)
+      const logController = require('../../src/controllers/log.controller')(namespace.MockService())
 
-        res.on('end', function() {
-          let data = JSON.parse(res._getData())      
-          expect(namespace.Service.calledOnce).to.equal(true)
-          res._isJSON().should.be.true
-          expect(data[0].code).to.equal('200')
-          expect(data[1].code).to.equal('208')
-          done()
-        })
+      res.on('end', function () {
+        let data = JSON.parse(res._getData())
 
+        res._isJSON().should.be.true
+        expect(namespace.MockService.calledOnce).to.equal(true)
+        expect(data.length).to.equal(2)
+        expect(data[0].code).to.equal(logArray[0].code)
+        expect(data[1].code).to.equal(logArray[1].code)
+        done()
+      })
 
-
-
-  logController.getAllLogs(req, res, next)  
-
+      logController.getAllLogs(req, res, next)
     }))
 
+    // /logs Error
     it('returns an error')
-  })
-
-
-
-
-
-
-
-  // api/auth
-  describe('Given a user requesting authentication', () => {
-    it('returns Bad Request (400) and will receive a json with error providing invalid credentials')
-    it('returns Internal Server Error (500) and will receive a json with error providing incomplete arguments')
-    it('returns Ok (200) and will receive a json with JWT token providing valid credentials')
   })
 
   // api/reset-password
@@ -86,18 +57,5 @@ describe('Log controller test', () => {
     it('returns Not Found (404) for non-existing users')
     it('returns Ok (200) with json containing email confirmation providing a valid email')
   })
-
-  // api/confirm-reset-password/:token
-  describe('Given a user requesting to reset password via email confirmation', () => {
-    it('returns Not Found (404) if token is not inside route')
-    it('returns Ok (200) with reset-password page')
-  })
-
-  // api/complete-reset-password
-  describe('Given a user resetting password via reset-password page', () => {
-    it('returns Unprocessable Entity (422) providing an invalid token')
-    it('returns Bad Request (400) providing invalid current password')
-    it('returns Bad Request (400) providing invalid confirmation password')
-    it('returns Ok (200) with json containing email confirmation providing a valid request')
-  })
+  
 })
