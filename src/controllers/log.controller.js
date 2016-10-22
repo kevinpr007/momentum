dfe00339b3cef7d7296f8a790a4e07b7b8c8dc54
Kevin Rivera <kevinpr_007@hotmail.com>
2016-10-22 13:41:53 -0400

    Reformat code

--- src/controllers/log.controller.js
+++ src/controllers/log.controller.js
@@ -3,28 +3,20 @@ const HttpStatus = require('http-status-codes')
 let logController = (logService) => {
   let getAllLogs = (req, res, next) => {
     logService.getAll()
-    .then(logs => {
-      return res.status(HttpStatus.OK).json(logs)
-    })
-    .catch(err => {
-      next(err)
-    })
+    .then(logs => res.status(HttpStatus.OK).json(logs))
+    .catch(err => next(err))
   }
 
   let getByCode = (req, res, next) => {
     logService.getByCode(req.params.code)
     .then(logs => res.status(HttpStatus.OK).json(logs))
-    .catch(err => {
-      next(err)
-    })
+    .catch(err => next(err))
   }
 
   let getByStatus = (req, res, next) => {
     logService.getByStatus(req.params.status)
     .then(logs => res.status(HttpStatus.OK).json(logs))
-    .catch(err => {
-      next(err)
-    })
+    .catch(err => next(err))
   }
 
   return {
