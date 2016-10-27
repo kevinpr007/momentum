e66609e8299fa501bf901d68d8267885c79cfd18
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-10-27 14:08:01 -0400

    Email service test
    Refactoring tests to provide more readability and organization.

--- test/services/email.service.spec.js
+++ test/services/email.service.spec.js
@@ -1,32 +1,38 @@
 describe('Email service test', () => {
-  describe('Given a basic information email template', () => {
 
-    const transporter = require('../../src/config/nodemailer')
-    const emailService = require('../../src/services/email.service')
+  const transporter = require('../../src/config/nodemailer')
+  const emailService = require('../../src/services/email.service')
 
-    let template = {
-      from: 'from@stylepoint.com',
-      to: 'to@stylepoint.com',
-      subject: 'Subject Email Test',
-      html: `<p>Test Email</p>`
-    }
-
-    it('will generate an email successfuly', sinon.test(function (done) {
+  describe('Given an email template', () => {
+    context('when proper smtp configuration is provided', () => {
+      it('will generate an email successfully', sinon.test(function (done) {
+        let template = {
+          from: 'from@stylepoint.com',
+          to: 'to@stylepoint.com',
+          subject: 'Subject Email Test',
+          html: `<p>Test Email</p>`
+        }
 
         this.stub(transporter, 'sendMail').yields(null, {success: true})
 
         emailService(template).send().then(result => {
           assert.notEqual(result, null)
-          expect(result).to.be.an('object');;;
+          expect(result).to.be.an('object')
           expect(result.success).to.be.equal(true)
           expect(transporter.sendMail.callCount).to.equal(1)
           done()
         }).catch(err => done(err))
-      })
-    )
+      }))
+    })
 
-    it('will NOT generate an email successfuly', sinon.test(function (done) {
-
+    context('when there are smtp issues', () => {
+      it('will throw an error', sinon.test(function (done) {
+        let template = {
+          from: 'from@stylepoint.com',
+          to: 'to@stylepoint.com',
+          subject: 'Subject Email Test',
+          html: `<p>Test Email</p>`
+        }
         let result = new Error('Error Message')
         result.success = false
 
@@ -34,13 +40,12 @@ describe('Email service test', () => {
 
         emailService(template).send().catch(err => {
           assert.notEqual(err, null)
-          expect(err).to.be.an('Error');
+          expect(err).to.be.an('Error')
           expect(err.success).to.be.equal(false)
           expect(transporter.sendMail.callCount).to.equal(1)
           done()
         })
-      })
-    )
-
+      }))
+    })
   })
 })
