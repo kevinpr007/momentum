85705eb1bff9f39b7da7fa42956b675d94a3fff4
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-10-27 13:34:35 -0400

    Log model tests
    Refactoring tests to provide more readability and organization.

--- test/models/logs.server.model.spec.js
+++ test/models/logs.server.model.spec.js
@@ -1,16 +1,20 @@
 describe('Logs schema validation tests', () => {
-  describe('Given a Logs model', () => {
-    let LogModel = require('../../src/models/logs.server.model.js')
 
-    it('Using the getError() function should return CODE - STATUS - MESSAGE', (done) => {
-      let log = new LogModel({
-        code: '400',
-        status: 'error',
-        message: 'Not Found'
+  let Log = require('../../src/models/logs.server.model.js')
+
+  describe('Given a Log instance', () => {
+
+    context('when using getError() function', () => {
+      it('will return CODE - STATUS - MESSAGE', (done) => {
+        let log = new Log({
+          code: '400',
+          status: 'error',
+          message: 'Not Found'
+        })
+
+        expect(log.getError).to.be.equal('400 - error - Not Found')
+        done()
       })
-
-      expect(log.getError).to.be.equal('400 - error - Not Found')
-      done()
     })
   })
 })
