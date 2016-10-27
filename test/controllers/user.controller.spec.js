279aa135220f6165c5b97550ad7444fdac42c69e
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-10-27 16:14:42 -0400

    User controller tests
    Refactoring tests to provide more readability and organization.

--- test/controllers/user.controller.spec.js
+++ test/controllers/user.controller.spec.js
@@ -14,76 +14,78 @@ describe('User entity requests', () => {
     userService = require('../../src/services/user.service')
   })
 
-  describe('Given a request to get all existing users', () => {
-    it('returns Ok (200) with json array containing all existing users providing a valid request', sinon.test(function (done) {
-      let users = [new User(), new User()]
-      req.method = 'GET'
-      req.url = 'api/users'
-      next = args => done(args)
+  describe('Given a request to User resource', () => {
+    context('when requesting to retrieve all users', () => {
+      it('returns Ok (200) with json array containing all existing users', sinon.test(function (done) {
+        let users = [new User(), new User()]
+        req.method = 'GET'
+        req.url = 'api/users'
+        next = args => done(args)
 
-      userService = this.stub(userService())
-      userService.getAll.resolves(users)
+        userService = this.stub(userService())
+        userService.getAll.resolves(users)
 
-      userController(userService).getAllUsers(req, res, next)
+        userController(userService).getAllUsers(req, res, next)
 
-      res.on('end', () => {
-        let data = JSON.parse(res._getData())
-        expect(data.length).to.equal(2)
-        expect(res.statusCode).to.equal(200)
-        assert.isTrue(userService.getAll.calledOnce)
-        done()
-      })
-    }))
-  })
+        res.on('end', () => {
+          let data = JSON.parse(res._getData())
+          expect(data.length).to.equal(2)
+          expect(res.statusCode).to.equal(200)
+          assert.isTrue(userService.getAll.calledOnce)
+          done()
+        })
+      }))
+    })
 
-  describe('Given a request to get a user by username', () => {
-    it('returns Not Found (404) providing a non-existing username', sinon.test(function (done) {
-      req.method = 'GET'
-      req.url = 'api/users'
-      req.params = {
-        userName: 'test@dev.com'
-      }
+    context('when requesting a user by username (email)', () => {
+      it('returns Not Found (404) providing a non-existing username', sinon.test(function (done) {
+        req.method = 'GET'
+        req.url = 'api/users'
+        req.params = {
+          userName: 'test@dev.com'
+        }
 
-      userService = this.stub(userService())
-      userService.getByEmail.resolves(null)
+        userService = this.stub(userService())
+        userService.getByEmail.resolves(null)
 
-      userController(userService).getByUserEmail(req, res, next)
+        userController(userService).getByUserEmail(req, res, next)
 
-      function next (args) {
-        try {
-          expect(args).to.be.an('Error')
-          expect(args.status).to.equal(404)
+        function next (args) {
+          try {
+            expect(args).to.be.an('Error')
+            expect(args.status).to.equal(404)
+            assert.isTrue(userService.getByEmail.calledOnce)
+            done()
+          } catch (err) {
+            done(err)
+          }
+        }
+      }))
+
+      it('returns Ok (200) with json containing user providing a valid username', sinon.test(function (done) {
+        let user = new User({
+          email: 'test@dev.com'
+        })
+        req.method = 'GET'
+        req.url = 'api/users'
+        req.params = {
+          userName: 'test@dev.com'
+        }
+        next = args => done(args)
+
+        userService = this.stub(userService())
+        userService.getByEmail.resolves(user)
+
+        userController(userService).getByUserEmail(req, res, next)
+
+        res.on('end', () => {
+          let data = JSON.parse(res._getData())
+          expect(res.statusCode).to.equal(200)
+          expect(data).to.have.property('email', user.email)
           assert.isTrue(userService.getByEmail.calledOnce)
           done()
-        } catch (err) {
-          done(err)
-        }
-      }
-    }))
-
-    it('returns Ok (200) with json containing user providing a valid username', sinon.test(function (done) {
-      let user = new User({
-        email: 'test@dev.com'
-      })
-      req.method = 'GET'
-      req.url = 'api/users'
-      req.params = {
-        userName: 'test@dev.com'
-      }
-      next = args => done(args)
-
-      userService = this.stub(userService())
-      userService.getByEmail.resolves(user)
-      
-      userController(userService).getByUserEmail(req, res, next)
-
-      res.on('end', () => {
-        let data = JSON.parse(res._getData())
-        expect(res.statusCode).to.equal(200)
-        expect(data).to.have.property('email', user.email)
-        assert.isTrue(userService.getByEmail.calledOnce)
-        done()
-      })
-    }))
+        })
+      }))
+    })
   })
 })
