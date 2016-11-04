58f277d61319bfe75372c310479ef698c679aa2a
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-04 11:44:34 -0400

    User controller
    Updating getAllUsers() with pagedResult object.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,26 +1,26 @@
 const HttpStatus = require('http-status-codes')
+const pagedResult = require('../util/paged-result')
 const config = require('../config/config')().getVariable()
 
 let userController = userService => {
   let getAllUsers = (req, res, next) => {
-    userService.getAll(req.query.page).then(result => {
-      result.unshift(Math.ceil(result[0] / req.query.pageSize || parseInt(config.PAGE_SIZE)))
-      result.push(req.query.page || 0)
-      res.data = result
-      next()
+    let page = req.query.page || 0
+    let pageSize = req.query.pageSize || parseInt(config.PAGE_SIZE)
+    userService.getAll(page, pageSize).then(result => {
+      let data = pagedResult(page, pageSize, result)
+      return res.status(HttpStatus.OK).json(data)
     }).catch(next)
   }
 
   let getByUserEmail = (req, res, next) => {
-    userService.getByEmail(req.params.userName)
-        .then(user => {
-          if (!user) {
-            let err = new Error('User not found.')
-            err.status = HttpStatus.NOT_FOUND
-            throw err
-          }
-          res.status(HttpStatus.OK).json(user)
-        }).catch(next)
+    userService.getByEmail(req.params.userName).then(user => {
+      if (!user) {
+        let err = new Error('User not found.')
+        err.status = HttpStatus.NOT_FOUND
+        throw err
+      }
+      res.status(HttpStatus.OK).json(user)
+    }).catch(next)
   }
 
   return {
