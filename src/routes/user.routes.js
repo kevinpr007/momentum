fe470f58a95d85af6a48ad9bb6d7e2a2e623e5b5
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-03 22:45:22 -0400

    User routes update
    Adding hypermedia middleware to /users route.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const userService = require('../services/user.service')()
 const userController = require('../controllers/user.controller')(userService)
 const auth = require('passport').authenticate('jwt', {session: false})
+const setHypermedia = require('../util/user.hypermedia')
 
 module.exports = router => {
-  router.get('/users', auth, userController.getAllUsers)
+  router.get('/users', auth, userController.getAllUsers, setHypermedia)
   router.get('/users/:userName', auth, userController.getByUserEmail)
 }
