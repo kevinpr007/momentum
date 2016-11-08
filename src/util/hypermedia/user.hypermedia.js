ba40378e70c36987535056b815e591f4fd752426
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-08 11:32:10 -0400

    User hypermedia update
    Updating userHypermedia() with route map instance.

--- src/util/hypermedia/user.hypermedia.js
+++ src/util/hypermedia/user.hypermedia.js
@@ -1,27 +1,31 @@
 const _ = require('lodash')
 
-let setHypermedia = (req, user) => {
-  user.links = []
-  user.links.push({
-    href: (() => {
-      req.originalUrl = req.originalUrl.replace(user.email, '')
-      return `${req.protocol}://${req.headers.host}${req.originalUrl}${user.email}`
-    })(),
-    rel: 'self',
-    method: 'GET'
-  })
-  return user
-}
+module.exports = router => {
+  let setLinks = (req, user, map) => {
+    user.links = []
+    user.links.push({
+      href: `${req.protocol}://${req.headers.host}${map.get('getAllUsers').path}/${user.email}`,
+      rel: 'self',
+      method: map.get('getAllUsers').method
+    })
+    return user
+  }
 
-module.exports = (req, res, next) => {
-  try {
-    if ('data' in res.body) {
-      res.body.data = _.each(res.body.data, elem => setHypermedia(req, elem._doc))
-    } else {
-      res.body = setHypermedia(req, res.body._doc)
+  let setResponse = (req, res, next) => {
+    try {
+      let routeMap = require('../route-map')(router)
+      if ('data' in res.body) {
+        res.body.data = _.each(res.body.data, elem => setLinks(req, elem._doc, routeMap))
+      } else {
+        res.body = setLinks(req, res.body._doc, routeMap)
+      }
+      res.json(res.body)
+    } catch (err) {
+      next(err)
     }
-    res.json(res.body)
-  } catch (err) {
-    next(err)
+  }
+
+  return {
+    setResponse: setResponse
   }
 }
