50063f78148cac9b23f50b6fb27540631d968652
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-10 23:08:47 -0400

    Hypermedia config
    Updating hypermedia config structure.

--- src/util/hypermedia/hypermedia.config.js
+++ src/util/hypermedia/hypermedia.config.js
@@ -1,46 +1,40 @@
 const mongoDB = require('../../config/mongoose.collections.json')
 const routes = require('../../routes/routes.config')
 const _ = require('lodash')
-const REGEX = /\/:\w+/
 
-module.exports = () => {
-  let setLinks = (req, entity, model) => {
-    let baseUrl = `${req.protocol}://${req.headers.host}`
-    return getLinks(entity, baseUrl, routes, model)
+class Hypermedia {
+  constructor (req, model) {
+    this.baseUrl = `${req.protocol}://${req.headers.host}`
+    this.model = model
+    this.REGEX = /\/:\w+/
   }
 
-  let getLinks = (entity, baseUrl, routes, model) => {
-    switch (model) {
+  setLinks (entity) {
+    switch (this.model) {
       case mongoDB.Model.Log:
-        require('./log.hypermedia.js')(entity, baseUrl, routes, REGEX)
+        require('./log.hypermedia.js')(entity, this.baseUrl, routes, this.REGEX)
         break
-
-      case mongoDB.Model.User:        
-        require('./user.hypermedia.js')(entity, baseUrl, routes, REGEX)
+      case mongoDB.Model.User:
+        require('./user.hypermedia.js')(entity, this.baseUrl, routes)
         break
-
       default:
         throw new Error('Entity not defined')
     }
     return entity
   }
 
-  let setResponse = (req, entity, next) => {
+  setResponse (entity, next) {
     try {
       if ('data' in entity) {
-        let model = entity.data[0].constructor.modelName
-        entity.data = _.each(entity.data, item => setLinks(req, item._doc, model))
+        entity.data = _.each(entity.data, item => this.setLinks(item._doc))
       } else {
-        let model = entity.constructor.modelName
-        entity._doc = setLinks(req, entity._doc, model)
+        entity._doc = this.setLinks(entity._doc)
       }
       return entity
     } catch (err) {
       next(err)
     }
   }
-
-  return {
-    setResponse: setResponse
-  }
 }
+
+module.exports = Hypermedia
