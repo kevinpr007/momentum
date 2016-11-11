d038282d3cb741e18d9294f980cc90a9d819ff3d
Kevin Rivera <kevinpr_007@hotmail.com>
2016-11-11 18:43:05 -0400

    Test the links generated for a particular entity

--- test/util/hypermedia.config.spec.js
+++ test/util/hypermedia.config.spec.js
@@ -11,8 +11,8 @@ describe('Hypermedia tests', () => {
 
   describe('Given a base information to set a hyperlink', () => {
     context('when passing a list of entities', () => {
-      it.only('will generate the related links for that particular records', sinon.test(function (done) {
-        let entity = {
+      it('will generate the related links for that particular records', sinon.test(function (done) {
+        let entities = {
           data: [
             new Log({code: '200'}),
             new Log({code: '200'})
@@ -22,11 +22,26 @@ describe('Hypermedia tests', () => {
         let hl = new Hypermedia(req)
         let next = args => done(args)
 
+        entities = hl.setResponse(entities, next)
+
+        expect(entities.data).to.be.instanceof(Array)
+        expect(entities.data).to.have.length(2)
+        expect(entities.data[0]._doc).to.have.property('links')
+        done()
+      }))
+    })
+
+    context('when passing an entity', () => {
+      it.only('will generate the related links for that particular entity', sinon.test(function (done) {
+        let entity = new Log({code: '200'})
+
+        let hl = new Hypermedia(req)
+        let next = args => done(args)
+
         entity = hl.setResponse(entity, next)
 
-        expect(entity.data).to.be.instanceof(Array)
-        expect(entity.data).to.have.length(2)
-        expect(entity.data[0]._doc).to.have.property('links')
+        expect(entity).to.be.instanceof(Log)
+        expect(entity._doc).to.have.property('links')
         done()
       }))
     })
