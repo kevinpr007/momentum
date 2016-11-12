899cfcc9825d37bb491e2f96dae5637dfc9fa6d1
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-11 20:51:18 -0400

    Pagination update
    Updating pagination object with nextPage / prevPage url's.

--- src/util/pagination/paged-result.js
+++ src/util/pagination/paged-result.js
@@ -6,13 +6,19 @@ const DATA_FIELD = 1
  * @pagedResult: It is required that the result parameter contains the total count
  * of records at index 0 and the array of items at index 1.
  */
-module.exports = (page = 0, pageSize = parseInt(config.PAGE_SIZE), data) => {
+module.exports = (req, page = 0, pageSize = parseInt(config.PAGE_SIZE), data) => {
+
+  const REGEX = /(\?(page)=(\d+))/gi
+  let host = `${req.protocol}://${req.headers.host}`
+  let url = `${req.originalUrl.replace(REGEX, '')}`
+
   let totalPages = parseInt((data[TOTAL_COUNT_FIELD] / pageSize).toFixed())
+
   return {
     totalPages: totalPages,
     totalCount: data[TOTAL_COUNT_FIELD],
     data: data[DATA_FIELD],
-    prevPage: page > 1 ? page - 1 : null,
-    nextPage: page < totalPages ? parseInt(page) + 1 : null
+    prevPage: page > 1 ? `${host}${url}?page=${parseInt(page - 1)}` : null,
+    nextPage: page < totalPages ? `${host}${url}?page=${parseInt(page + 1)}` : null
   }
 }
