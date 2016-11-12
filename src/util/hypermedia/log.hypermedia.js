a6fc6a5e66ecdad40131a15f000ac1b1c686be96
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-11 21:00:06 -0400

    Log hypermedia update
    Updating hypermedia links.

--- src/util/hypermedia/log.hypermedia.js
+++ src/util/hypermedia/log.hypermedia.js
@@ -1,21 +1,19 @@
 const routes = require('../../routes/routes.config')
 
 module.exports = (entity, baseUrl) => {
-  let regex = /\/:\w+/ // TODO: Remove after routes are standardized.
   // TODO: Business Logic
   return entity.links = [{
     href: `${baseUrl}${routes.get('getLogs').path}`,
-    rel: 'self',
+    rel: 'collection',
     method: routes.get('getLogs').method
-  },
-  {
-    href: `${baseUrl}${routes.get('getByLogCode').path}`.replace(regex, `/${entity.code}`),
-    rel: 'self',
+  }, {
+    href: `${baseUrl}${routes.get('getLogs').path}/${entity.code}`,
+    rel: 'code-filter',
     method: routes.get('getByLogCode').method
   },
   {
-    href: `${baseUrl}${routes.get('getByLogStatus').path}`.replace(regex, `/${entity.status}`),
-    rel: 'self',
+    href: `${baseUrl}${routes.get('getLogs').path}/${entity.status}`,
+    rel: 'status-filter',
     method: routes.get('getByLogStatus').method
   }]
 }
