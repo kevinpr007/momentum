ad6b62fc17554b2a4500368b59492955b794e56c
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-11 20:53:08 -0400

    User hypermedia
    Updating user hypermedia with 'collection' link.

--- src/util/hypermedia/user.hypermedia.js
+++ src/util/hypermedia/user.hypermedia.js
@@ -3,6 +3,10 @@ const routes = require('../../routes/routes.config')
 module.exports = (entity, baseUrl) => {
   // TODO: Business Logic
   return entity.links = [{
+    href: `${baseUrl}${routes.get('getUsers').path}`,
+    rel: 'collection',
+    method: routes.get('getUsers').method
+  }, {
     href: `${baseUrl}${routes.get('getUsers').path}/${entity.email}`,
     rel: 'self',
     method: routes.get('getUsers').method
