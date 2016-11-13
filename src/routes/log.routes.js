cf4d49470a8e2ce37aee5cef4197ed178c536044
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-13 14:32:50 -0400

    Log routes
    Adding authorize() middleware to log routes.

--- src/routes/log.routes.js
+++ src/routes/log.routes.js
@@ -1,9 +1,11 @@
 const logService = require('../services/log.service')()
 const logController = require('../controllers/log.controller')(logService)
+const authenticate = require('passport').authenticate('jwt', {session: false})
+const authorize = require('../services/auth.service')().authorize
 const routes = require('./routes.config')
 
 module.exports = router => {
-  router.get(routes.get('getLogs').path, logController.getAllLogs)
-  router.get(routes.get('getByLogCode').path, logController.getByCode)
-  router.get(routes.get('getByLogStatus').path, logController.getByStatus)
+  router.get(routes.get('getLogs').path, authenticate, authorize(['sysAdmin']), logController.getAllLogs)
+  router.get(routes.get('getByLogCode').path, authenticate, authorize(['sysAdmin']), logController.getByCode)
+  router.get(routes.get('getByLogStatus').path, authenticate, authorize(['sysAdmin']), logController.getByStatus)
 }
