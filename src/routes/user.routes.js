231d135228602d8d8370b29edcbd0a1a1c53e6cc
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-11-13 14:33:03 -0400

    User routes
    Adding authorize() middleware to user routes.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 const userService = require('../services/user.service')()
 const userController = require('../controllers/user.controller')(userService)
-const auth = require('passport').authenticate('jwt', {session: false})
+const authenticate = require('passport').authenticate('jwt', {session: false})
+const authorize = require('../services/auth.service')().authorize
 const routes = require('./routes.config')
 
 module.exports = router => {
-  router.get(routes.get('getUsers').path, auth, userController.getAllUsers)
-  router.get(routes.get('getByUserEmail').path, auth, userController.getByUserEmail)
+  router.get(routes.get('getUsers').path, authenticate, authorize(['Admin']), userController.getAllUsers)
+  router.get(routes.get('getByUserEmail').path, authenticate, authorize(['Admin']), userController.getByUserEmail)
 }
