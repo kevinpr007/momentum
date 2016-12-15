98ee34335c0e24426c3caf73c2941a7cdda0e333
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-12-15 15:57:08 -0400

    User docs update
    Changing documentation to routes instead of controllers.
    
    Additional changes:
    Updating npm docs script.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,62 @@ const authorize = require('../services/auth.service')().authorize
 const routes = require('./routes.config')
 
 module.exports = router => {
-  router.get(routes.get('getUsers').path, authenticate, authorize(['Admin']), userController.getAllUsers)
-  router.get(routes.get('getByUserEmail').path, authenticate, authorize(['Admin']), userController.getByUserEmail)
+  /**
+  * @api {get} /users?page={page}&pageSize={pageSize} Get all users
+  * @apiVersion 0.0.1
+  * @apiName getAllUsers
+  * @apiGroup Users
+  *
+  * @apiParam {Number} page Optional page number. Default value: 0
+  * @apiParam {Number} pageSize Optional page size. Default value: 10
+  *
+  * @apiSuccess {String} firstName User's first name.
+  * @apiSuccess {String} lastName  User's last name.
+  *
+  * @apiSuccessExample Success-Response:
+  *     HTTP/1.1 200 OK
+  *     {
+  *       "firstname": "John",
+  *       "lastname": "Doe"
+  *     }
+  *
+  * @apiError NotFound User not found.
+  *
+  * @apiErrorExample Error-Response:
+  *     HTTP/1.1 404 Not Found
+  *     {
+  *       "error": "User not found"
+  *     }
+  **/
+  router.get(routes.get('getUsers').path, authenticate,
+    authorize(['Admin']), userController.getAllUsers)
+
+  /**
+  * @api {get} /users/:email Get user by email
+  * @apiVersion 0.0.1
+  * @apiName getByUserEmail
+  * @apiGroup Users
+  *
+  * @apiParam {String} email User's email.
+  *
+  * @apiSuccess {String} firstName User's first name.
+  * @apiSuccess {String} lastName  User's last name.
+  *
+  * @apiSuccessExample Success-Response:
+  *     HTTP/1.1 200 OK
+  *     {
+  *       "firstname": "John",
+  *       "lastname": "Doe"
+  *     }
+  *
+  * @apiError NotFound User not found.
+  *
+  * @apiErrorExample Error-Response:
+  *     HTTP/1.1 404 Not Found
+  *     {
+  *       "error": "User not found"
+  *     }
+  **/
+  router.get(routes.get('getByUserEmail').path, authenticate,
+    authorize(['Admin']), userController.getByUserEmail)
 }
