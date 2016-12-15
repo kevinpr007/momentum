8167cf4284122b6586dea178d577a52cc809add2
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2016-12-15 15:36:53 -0400

    User Controller
    Adding initial documentation for getUserByEmail and getAllUsers.
    
    Additional changes:
    Updating .gitignore.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,34 @@ const getPageValidations = require('../util/pagination/page-validations')
 const config = require('../config/config')()
 
 let userController = userService => {
+
+  /**
+  * @api {get} /users?page={page}&pageSize={pageSize} Get all users
+  * @apiVersion 0.0.1
+  * @apiName getAllUsers
+  * @apiGroup User
+  *
+  * @apiParam {Number} page Optional page number. Default value: 0
+  * @apiParam {Number} pageSize Optional page size. Default value: 10
+  *
+  * @apiSuccess {String} firstName User's first name.
+  * @apiSuccess {String} lastName  User's last name.
+  *
+  * @apiSuccessExample Success-Response:
+  *     HTTP/1.1 200 OK
+  *     {
+  *       "firstname": "John",
+  *       "lastname": "Doe"
+  *     }
+  *
+  * @apiError NotFound User not found.
+  *
+  * @apiErrorExample Error-Response:
+  *     HTTP/1.1 404 Not Found
+  *     {
+  *       "error": "User not found"
+  *     }
+  **/
   let getAllUsers = (req, res, next) => {
     let page = parseInt(req.query.page || 0)
     let pageSize = parseInt(req.query.pageSize || config.PAGE_SIZE)
@@ -17,6 +45,32 @@ let userController = userService => {
     }).catch(next)
   }
 
+  /**
+  * @api {get} /users/:email Get user information
+  * @apiVersion 0.0.1
+  * @apiName getByUserEmail
+  * @apiGroup User
+  *
+  * @apiParam {String} email User's email.
+  *
+  * @apiSuccess {String} firstName User's first name.
+  * @apiSuccess {String} lastName  User's last name.
+  *
+  * @apiSuccessExample Success-Response:
+  *     HTTP/1.1 200 OK
+  *     {
+  *       "firstname": "John",
+  *       "lastname": "Doe"
+  *     }
+  *
+  * @apiError NotFound User not found.
+  *
+  * @apiErrorExample Error-Response:
+  *     HTTP/1.1 404 Not Found
+  *     {
+  *       "error": "User not found"
+  *     }
+  **/
   let getByUserEmail = (req, res, next) => {
     userService.getByEmail(req.params.userName).then(user => {
       if (!user) {
