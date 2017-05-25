891dcab903bfb775d0643d1039b4bcc89ce53d47
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-05-25 16:22:30 -0400

    temp. changes to workshifts by all users query.

--- references/queries/workshifts-by-all-users.js
+++ references/queries/workshifts-by-all-users.js
@@ -7,8 +7,8 @@
  * https://docs.mongodb.com/manual/reference/operator/aggregation/first/#first-aggregation
  * https://docs.mongodb.com/manual/reference/operator/aggregation/project/#project-aggregation
  * https://docs.mongodb.com/manual/reference/operator/aggregation/filter/#filter-aggregation
+ * https://docs.mongodb.com/manual/reference/operator/aggregation/cond/#cond-aggregation
  */
-
 db.getCollection('m_application').aggregate([
   { $limit: 1 },
   {
@@ -28,11 +28,12 @@ db.getCollection('m_application').aggregate([
       as: 'workshifts'
     }
   },
+  { $unwind: '$workshifts' },
   {
     $sort: {
       'users.firstName': 1,
       'users.lastName': 1,
-      'workshifts.startDate': -1
+      'workshifts.startDate': 1
     }
   },
   {
@@ -47,10 +48,12 @@ db.getCollection('m_application').aggregate([
         email: 1,
         roles: 1,
         workshifts: {
-          $filter: {
-            input: '$workshifts',
-            as: 'wf',
-            cond: { $eq: [ '$$wf.userId', '$users._id' ] }
+          $cond: {
+            if: {
+              $eq: ['$workshifts.userId', '$users._id']
+            },
+            then: '$workshifts',
+            else: null
           }
         }
       }
@@ -58,10 +61,39 @@ db.getCollection('m_application').aggregate([
   },
   {
     $group: {
-      _id: '$_id',
-      name: { $first: '$name' },
-      apptypeId: { $first: '$appTypeId' },
-      users: { $push: '$users' }
+      _id: {
+        userId: '$users.workshifts.userId',
+        name: '$name',
+        appId: '$_id',
+        appTypeId: '$appTypeId',
+        users: '$users'
+      }
+    }
+  },
+  {
+    $group: {
+      _id: '$_id.users._id',
+      appId: { $first: '$_id.appId' },
+      appTypeId: { $first: '$_id.appTypeId' },
+      userId: { $first: '$_id.users._id' },
+      firstName: { $first: '$_id.users.firstName' },
+      lastName: { $first: '$_id.users.lastName' },
+      email: { $first: '$_id.users.email' },
+      roles: { $first: '$_id.users.roles' },
+      workshifts: { $push: '$_id.users.workshifts' }
+    }
+  },
+  { // $project to remove additional _id property
+    $project: {
+      _id: 0,
+      appId: 1,
+      appTypeId: 1,
+      userId: 1,
+      firstName: 1,
+      lastName: 1,
+      email: 1,
+      roles: 1,
+      workshifts: 1
     }
   }
 ])
