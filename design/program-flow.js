1a08c167d6f74db57b11abb04198a27b9045aa96
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-05-26 14:14:54 -0400

    chore(db): adding services by users query.

--- design/program-flow.js
+++ design/program-flow.js
@@ -369,16 +369,70 @@ function runQueries () {
         }
       },
       {
-        $match: {
-          'users.roles.name': 'Admin'
+        $project: {
+          _id: 1,
+          name: 1,
+          appTypeId: 1,
+          users: {
+            _id: 1,
+            firstName: 1,
+            lastName: 1,
+            email: 1,
+            roles: 1,
+            services: {
+              $cond: {
+                if: {
+                  $eq: ['$services.userId', '$users._id']
+                },
+                then: '$services',
+                else: null
+              }
+            }
+          }
+        }
+      },
+      {
+        $group: {
+          _id: {
+            userId: '$users.services.userId',
+            name: '$name',
+            appId: '$_id',
+            appTypeId: '$appTypeId',
+            users: '$users'
+          }
+        }
+      },
+      {
+        $group: {
+          _id: '$_id.users._id',
+          name: { $first: '$_id.name' },
+          appId: { $first: '$_id.appId' },
+          appTypeId: { $first: '$_id.appTypeId' },
+          userId: { $first: '$_id.users._id' },
+          firstName: { $first: '$_id.users.firstName' },
+          lastName: { $first: '$_id.users.lastName' },
+          email: { $first: '$_id.users.email' },
+          roles: { $first: '$_id.users.roles' },
+          services: { $push: '$_id.users.services' }
+        }
+      },
+      {
+        $group: {
+          _id: '$appId',
+          name: { $first: '$name' },
+          appTypeId: { $first: '$appTypeId' },
+          users: {
+            $addToSet: {
+              _id: '$userId',
+              firstName: '$firstName',
+              lastName: '$lastName',
+              email: '$email',
+              roles: '$roles',
+              services: '$services'
+            }
+          }
         }
       }
-      //              {
-      //         $group: {
-      //           '??': '$?????',
-      //           'services': { $push: '$services' }
-      //         }
-      //       }
     ])
   ])
 
