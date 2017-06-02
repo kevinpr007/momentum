294089d2cef65384830119e1dd4e8a0fc6d79d74
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-06-01 23:41:56 -0400

    schedules by all users refactor :flushed:

--- references/queries/shedules-by-all-users.js
+++ references/queries/shedules-by-all-users.js
@@ -1,97 +1,58 @@
 /**
- * Retrieve all schedule by all users for an specific application ordered by
+ * Retrieve all schedules by all users for a specific application ordered by
  * user name and schedule time ascending
  */
-db.getCollection('m_application').aggregate([
-    { $limit: 1 },
+db.getCollection('m_user').aggregate([
   {
-    $lookup: {
-      from: 'm_user',
-      localField: '_id',
-      foreignField: 'roles.appId',
-      as: 'users'
+    $match: {
+      'roles.name': 'Admin'
     }
   },
-    { $unwind: '$users' },
+  { $unwind: '$roles' },
   {
     $lookup: {
       from: 'm_schedule',
-      localField: 'users._id',
+      localField: '_id',
       foreignField: 'userId',
       as: 'schedules'
     }
   },
-    { $unwind: '$schedules' },
+  { $unwind: '$schedules' },
   {
     $sort: {
-      'users.firstName': 1,
-      'users.lastName': 1,
+      'firstName': 1,
+      'lastName': 1,
       'schedules.startDate': 1
-    }
-  },
-  {
-    $project: {
-      _id: 1,
-      name: 1,
-      appTypeId: 1,
-      users: {
-        _id: 1,
-        firstName: 1,
-        lastName: 1,
-        email: 1,
-        roles: 1,
-        schedules: {
-          $cond: {
-            if: {
-              $eq: ['$schedules.userId', '$users._id']
-            },
-            then: '$schedules',
-            else: null
-          }
-        }
-      }
     }
   },
   {
     $group: {
       _id: {
-        userId: '$users.schedules.userId',
-        name: '$name',
-        appId: '$_id',
-        appTypeId: '$appTypeId',
-        users: '$users'
-      }
-    }
-  },
-  {
-    $group: {
-      _id: '$_id.users._id',
-      name: { $first: '$_id.name' },
-      appId: { $first: '$_id.appId' },
-      appTypeId: { $first: '$_id.appTypeId' },
-      userId: { $first: '$_id.users._id' },
-      firstName: { $first: '$_id.users.firstName' },
-      lastName: { $first: '$_id.users.lastName' },
-      email: { $first: '$_id.users.email' },
-      roles: { $first: '$_id.users.roles' },
-      schedules: { $push: '$_id.users.schedules' }
-    }
-  },
-  {
-    $group: {
-      _id: '$appId',
-      name: { $first: '$name' },
-      appTypeId: { $first: '$appTypeId' },
-      users: {
-        $addToSet: {
-          _id: '$userId',
+          appId: '$roles.appId',
+          userId: '$_id',
           firstName: '$firstName',
           lastName: '$lastName',
           email: '$email',
-          roles: '$roles',
-          schedules: '$schedules'
+          address: '$address'
+      },
+      roles: { $addToSet: '$roles' },
+      schedules: { $addToSet: '$schedules' }
+    }
+  },
+  {
+    $group: {
+        _id: '$_id.appId',
+        users: { 
+            $addToSet: {
+                userId: '$_id.userId',
+                firstName: '$_id.firstName',
+                lastName: '$_id.lastName',
+                email: '$_id.email',
+                address: '$_id.address',
+                roles: '$roles',
+                schedules: '$schedules',
+            }
         }
-      }
     }
   }
 ])
