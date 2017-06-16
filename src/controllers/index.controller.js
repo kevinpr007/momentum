c9fd532975c7d0b888d82951eed6f054773b08ba
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-06-16 15:52:48 -0400

    WIP: index controller. Returning temporary json response.

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,14 @@
-let indexController = (templateModel) => {
-  let index = (req, res, next) => {
-    res.render('index', templateModel)
-  }
+const HttpStatus = require('http-status-codes')
+
+let indexController = () => {
+  /**
+   * TODO:
+   * return swagger main page once implemented.
+   */
+  let index = (req, res, next) =>
+    res.status(HttpStatus.OK).json({
+      message: 'success'
+    })
 
   return {
     index
