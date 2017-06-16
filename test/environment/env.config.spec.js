b8fc1538ce8680c2ef56ecaae7f37eb9fbdea0b9
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-06-16 15:59:58 -0400

    updating env config module tests. 100 code coverage :tada:

--- test/environment/env.config.spec.js
+++ test/environment/env.config.spec.js
@@ -13,5 +13,31 @@ describe('Environment variables test', () => {
         done()
       }))
     })
+
+    context('when passing "staging" as value', () => {
+      it('will set NODE_ENV to "staging"', sinon.test(function (done) {
+        process.env.NODE_ENV = 'staging'
+        clearModule('../../src/config/config')
+        let envVariable = require('../../src/config/config')
+
+        expect(envVariable.NODE_ENV).to.equal(process.env.NODE_ENV)
+        done()
+      }))
+    })
+
+    context('when passing a null or unknown value', () => {
+      it('will throw an error', sinon.test(function (done) {
+        process.env.NODE_ENV = 'unknown'
+
+        try {
+          clearModule('../../src/config/config')
+          require('../../src/config/config')
+        } catch (args) {
+          expect(args).to.be.a('Error')
+          expect(args.message).to.contain('Error setting the environment')
+          done()
+        }
+      }))
+    })
   })
 })
