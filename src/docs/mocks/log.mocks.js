b00ffe60258a1faae22be1d362c1a716e814b7ce
Kevin Rivera <kevinpr_007@hotmail.com>
2017-06-21 21:35:27 -0400

    Add Swagger Documentation to REST API

---
+++ src/docs/mocks/log.mocks.js
@@ -0,0 +1,87 @@
+module.exports = {
+  getAll (req, res) {
+    return res.json([
+      {
+        'totalPages': 6,
+        'totalCount': 62,
+        'data': [
+          {
+            'links': [
+              {
+                'method': 'GET',
+                'rel': 'collection',
+                'href': `${req.headers.host}\/api\/logs`
+              },
+              {
+                'method': 'GET',
+                'rel': 'code-filter',
+                'href': `${req.headers.host}\/api\/logs\/77479`
+              },
+              {
+                'method': 'GET',
+                'rel': 'status-filter',
+                'href': `${req.headers.host}\/api\/logs\/`
+              }
+            ],
+            '_id': '592dfb7d0448021a24cdbe7e',
+            'message': null,
+            'status': null,
+            'code': '77479',
+            'getError': '77479 - null - null'
+          },
+          {
+            'links': [
+              {
+                'method': 'GET',
+                'rel': 'collection',
+                'href': `${req.headers.host}\/api\/logs`
+              },
+              {
+                'method': 'GET',
+                'rel': 'code-filter',
+                'href': `${req.headers.host}\/api\/logs\/13770`
+              },
+              {
+                'method': 'GET',
+                'rel': 'status-filter',
+                'href': `${req.headers.host}\/api\/logs\/`
+              }
+            ],
+            '_id': '592dfb7d0448021a24cdbe77',
+            'message': null,
+            'status': null,
+            'code': '13770',
+            'getError': '13770 - null - null'
+          },
+          {
+            'links': [
+              {
+                'method': 'GET',
+                'rel': 'collection',
+                'href': `${req.headers.host}\/api\/logs`
+              },
+              {
+                'method': 'GET',
+                'rel': 'code-filter',
+                'href': `${req.headers.host}\/api\/logs\/48354`
+              },
+              {
+                'method': 'GET',
+                'rel': 'status-filter',
+                'href': `${req.headers.host}\/api\/logs\/`
+              }
+            ],
+            '_id': '592dac87e862b32b70e9cbbf',
+            'message': null,
+            'status': null,
+            'code': '48354',
+            'getError': '48354 - null - null'
+          }
+        ],
+        'prevPage': null,
+        'nextPage': `${req.headers.host}\/api\/logs?page=1`
+      }
+    ])
+  }
+}
+
---
+++ src/docs/swagger.js
@@ -0,0 +1,21 @@
+const swagger = require('swagger-express-router')
+const swaggerUi = require('swagger-ui-express')
+const swaggerConfig = require('./swagger.json')
+
+const path = require('path')
+const fs = require('fs')
+
+const useBasePath = true
+const showExplorer = false
+const options = {}
+const customCss = fs.readFileSync(path.resolve(__dirname, 'swagger-ui.css'), 'utf-8')
+
+const middlewares = {
+  'logs_mocks': require('./mocks/log.mocks')
+}
+
+module.exports = app => {
+  swagger.setUpRoutes(middlewares, app, swaggerConfig, useBasePath)
+  app.use('/api-docs', swaggerUi.serve,
+    swaggerUi.setup(swaggerConfig, showExplorer, options, customCss))
+}
