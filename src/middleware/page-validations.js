d699f1f4a8d2ef885546839e47ba63e521cb2394
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-07-09 17:11:06 -0400

    WIP: renaming middleware(s) folder. Page-validations module promoted to middleware.

--- src/middleware/page-validations.js
+++ src/middleware/page-validations.js
@@ -1,18 +1,24 @@
 const HttpStatus = require('http-status-codes')
-/**
- * TODO:
- * change to middleware
- */
-module.exports = (page, pageSize) => {
+const config = require('../config/config')
+
+module.exports = (req, res, next) => {
+  const page = parseInt(req.query.page || 0)
+  const pageSize = parseInt(req.query.pageSize || config.PAGE_SIZE)
+
   if (page === undefined || isNaN(page)) {
     const err = new Error('You must provide a page number')
     err.status = HttpStatus.INTERNAL_SERVER_ERROR
-    throw err
+    next(err)
   }
 
   if (pageSize === undefined || isNaN(pageSize)) {
     const err = new Error('Page size must be a number')
     err.status = HttpStatus.INTERNAL_SERVER_ERROR
-    throw err
+    next(err)
   }
+
+  req.query.page = page
+  req.query.pageSize = pageSize
+
+  next()
 }
