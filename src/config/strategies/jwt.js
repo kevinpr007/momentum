f02149ee525ab69c0aadb0f400afc8cacd6e6286
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-07-27 10:56:47 -0400

    WIP: adding 'X-Updated-JWT' header in response.

--- src/config/strategies/jwt.js
+++ src/config/strategies/jwt.js
@@ -2,21 +2,33 @@ const config = require('../config')
 const passport = require('passport')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+const {refreshToken} = require('../../services/auth.service')()
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeader(),
-  secretOrKey: config.SECRET
+  secretOrKey: config.SECRET,
+  passReqToCallback: true
 }
 
 module.exports = () => {
   const userService = require('../../services/user.service')()
 
-  passport.use(new JwtStrategy(jwtOptions, (payload, cb) => {
+  passport.use(new JwtStrategy(jwtOptions, (req, payload, cb) => {
+    const expDate = new Date(parseInt(`${payload.exp}000`))
+
     userService.getById(payload._doc._id).then(user => {
       if (!user) {
         return cb(null, false, {
           message: 'Unknown user'
         })
+      }
+
+      if (2 === 2) { // TODO: validate expDate remaining time.
+        return refreshToken(user)
+          .then(jwt => {
+            req.res.set('X-Updated-JWT', jwt)
+            return cb(null, user)
+          })
       } else {
         return cb(null, user)
       }
