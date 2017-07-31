1754f034089a4bb3e106ffd11d54e32d0ab4f585
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-07-31 09:17:56 -0400

    WIP: adding initial tests for Application controller module.

---
+++ test/controllers/application.controller.spec.js
@@ -0,0 +1,30 @@
+const httpMocks = require('node-mocks-http')
+const HttpStatus = require('http-status-codes')
+
+describe.skip('Application entity requests', () => {
+  let Aplication = require('../../src/models/application.model')
+//   let appController = require('../../src/controllers/application.controller')
+  let appService, req, res
+
+  beforeEach(() => {
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse({
+      eventEmitter: require('events').EventEmitter
+    })
+    appService = require('../../src/services/application.service')
+  })
+
+  describe('Given a request to register a new application', () => {
+    context('when leaving required fields empty', () => {
+      it('will return a Bad Request Error (400)', sinon.test(function (done) {
+        done()
+      }))
+    })
+
+    context('when providing a valid request', () => {
+      it('returns Created (201) with application object', sinon.test(function (done) {
+        done()
+      }))
+    })
+  })
+})
