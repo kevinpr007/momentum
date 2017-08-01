a5d134e08b2cd5840d39ca08ce1ea1273d4cba34
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-07-31 20:31:28 -0400

    auth service module code refactor.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,21 +1,23 @@
-const {SECRET, RANDOM_BYTES, TOKEN_LIFE} = require('../config/config')
+const {SECRET, EXP_SECONDS} = require('../config/config')
 const jwt = require('jsonwebtoken')
 const Promise = require('bluebird')
 const mongoose = require('mongoose')
 mongoose.Promise = Promise
 
 const User = require('../models/user.model')
-const tokenExpTime = parseInt(TOKEN_LIFE)
+const tokenExpTime = parseInt(EXP_SECONDS)
 const moment = require('moment')
 const _ = require('lodash')
 
 const randomBytes = Promise.promisify(require('crypto').randomBytes)
 const HOURS = 3600
+const BYTES = 48
 
 module.exports = () => {
-  const generateToken = user => jwt.sign(user, SECRET, {
-    expiresIn: tokenExpTime
-  })
+  const generateToken = user =>
+    jwt.sign(user, SECRET, {
+      expiresIn: tokenExpTime
+    })
 
   const getToken = user => `JWT ${generateToken(user)}`
 
@@ -30,7 +32,7 @@ module.exports = () => {
   const resetPasswordToken = user => {
     const date = new Date()
     user = _.extend(user, User)
-    return randomBytes(parseInt(RANDOM_BYTES))
+    return randomBytes(BYTES)
       .then(buffer => {
         user.resetPasswordToken = buffer.toString('hex')
         user.resetPasswordExpires = date.setSeconds(date.getSeconds() + tokenExpTime)
