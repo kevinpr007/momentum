bb27fbe0da567f595aa18902f67c67b7cb8eaf84
Ivan Marquez <ivan.marquez.gdev@gmail.com>
2017-08-03 13:29:17 -0400

    WIP: auth controller module code refactor.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -11,30 +11,29 @@ const emails = {
 
 module.exports = (authService, userService, emailService) => {
   function auth (req, res, next) {
-    const setResponse = user => {
+    const setJWT = user => {
       user._doc.jwt = {
         token: authService.getToken(user),
         expiresIn: authService.setExpirationDate()
       }
 
-      res.status(HttpStatus.OK)
-        .json(new Hypermedia(req)
-        .setResponse(user, next))
+      res.locals = user
+      next()
     }
 
-    const matchPassword = isMatch => {
+    const matchPassword = ({user, isMatch}) => {
       if (!isMatch) {
         let message = 'Authentication failed. User not found.'
         return util.generateError(message, HttpStatus.BAD_REQUEST)
       }
 
-      return setResponse
+      return user
     }
 
-    const validatePassword = user =>
+    const validatePassword = user => 
       user.isValidPassword(req.body.password)
         .then(matchPassword)
-        .then(cb => cb(user))
+        .then(setJWT)
         .catch(next)
 
     const validateUser = user => {
@@ -52,91 +51,107 @@ module.exports = (authService, userService, emailService) => {
       .catch(next)
   }
 
-  const register = (req, res, next) => userService.getByEmail(req.body.email)
-    .then(existingUser => {
-      if (existingUser) {
+  function register (req, res, next) {
+    const validateUser = user => {
+      if (user) {
         const err = new Error('That email address is already registered.')
         err.status = HttpStatus.UNPROCESSABLE_ENTITY
         throw err
       }
 
       return userService.registerUser(req.body)
-    })
-    .then(user => {
-      let token = authService.getToken(user)
-      let expiresIn = authService.setExpirationDate()
-      user._doc.jwt = { token, expiresIn }
+    }
 
-      res.status(HttpStatus.CREATED).json(new Hypermedia(req).setResponse(user, next))
-
-      const email = emails.newAccount(user, req.headers.host).getTemplate()
-      const emailInfo = emailFactory(user.email, email.subject, email.html).getInfo()
-
-      return emailService(emailInfo).send()
-    })
-    .catch(next)
-
-  const resetPassword = (req, res, next) => userService.getByEmail(req.body.email)
-    .then(user => {
-      if (!user) {
-        const err = new Error('Your request could not be processed as entered. User does not exist.')
-        err.status = HttpStatus.NOT_FOUND
-        throw err
+    const setResponse = user => {
+      user._doc.jwt = {
+        token: authService.getToken(user),
+        expiresIn: authService.setExpirationDate()
       }
-      return authService.resetPasswordToken(user)
-    })
-    .then(user => {
-      const host = req.headers.referer || `${req.protocol}://${req.headers.host}/`
-      const email = emails.confirmResetPassword(host, user.resetPasswordToken).getTemplate()
-      const emailInfo = emailFactory(user.email, email.subject, email.html).getInfo()
+
+      res.status(HttpStatus.CREATED)
+        .json(user)
+
+      return user
+    }
+
+    const sendEmail = user => {
+      let email = emails.newAccount(user, req.headers.host).getTemplate()
+      let emailInfo = emailFactory(user.email, email.subject, email.html).getInfo()
 
       return emailService(emailInfo).send()
-    })
-    .then(data => res.status(HttpStatus.OK).json({ data }))
-    .catch(next)
+    }
 
-  const newPassword = (req, res, next) => authService.findByPasswordToken(req.body.token)
-    .then(user => {
-      if (!user) {
-        const err = new Error('Invalid token. Please confirm this action through your email.')
-        err.status = HttpStatus.UNPROCESSABLE_ENTITY
-        throw err
-      }
-      return user.isValidPassword(req.body.currentPassword)
-        .then(isMatch => {
-          if (isMatch) {
-            return user.confirmPasswordValid(req.body.password, req.body.confirmPassword)
-          } else {
-            const err = new Error('Invalid password. Please validate your current password.')
-            err.status = HttpStatus.BAD_REQUEST
-            throw err
-          }
-        })
-        .then(isConfirmed => {
-          if (isConfirmed) {
-            user.password = req.body.confirmPassword
-            user.resetPasswordToken = undefined
-            user.resetPasswordExpires = undefined
-            return userService.upsertUser(user)
-          } else {
-            const err = new Error('Invalid confirmation password. Please validate your new password.')
-            err.status = HttpStatus.BAD_REQUEST
-            throw err
-          }
-        })
-    })
-    .then(user => {
-      const email = emails.resetPassword().getTemplate()
-      const emailInfo = emailFactory(user.email, email.subject, email.html).getInfo()
+    return userService.getByEmail(req.body.email)
+      .then(validateUser)
+      .then(setResponse)
+      .then(sendEmail)
+      .catch(next)
+  }
 
-      return emailService(emailInfo).send()
-    })
-    .then(data => res.status(HttpStatus.OK).json({ data }))
-    .catch(next)
+  const resetPassword = (req, res, next) => 
+    userService.getByEmail(req.body.email)
+      .then(user => {
+        if (!user) {
+          const err = new Error('Your request could not be processed as entered. User does not exist.')
+          err.status = HttpStatus.NOT_FOUND
+          throw err
+        }
+        return authService.resetPasswordToken(user)
+      })
+      .then(user => {
+        const host = req.headers.referer || `${req.protocol}://${req.headers.host}/`
+        const email = emails.confirmResetPassword(host, user.resetPasswordToken).getTemplate()
+        const emailInfo = emailFactory(user.email, email.subject, email.html).getInfo()
+
+        return emailService(emailInfo).send()
+      })
+      .then(data => res.status(HttpStatus.OK).json({ data }))
+      .catch(next)
+
+  const newPassword = (req, res, next) => 
+    authService.findByPasswordToken(req.body.token)
+      .then(user => {
+        if (!user) {
+          const err = new Error('Invalid token. Please confirm this action through your email.')
+          err.status = HttpStatus.UNPROCESSABLE_ENTITY
+          throw err
+        }
+        return user.isValidPassword(req.body.currentPassword)
+          .then(({isMatch}) => {
+            if (isMatch) {
+              return user.confirmPasswordValid(req.body.password, req.body.confirmPassword)
+            } else {
+              const err = new Error('Invalid password. Please validate your current password.')
+              err.status = HttpStatus.BAD_REQUEST
+              throw err
+            }
+          })
+          .then(isConfirmed => {
+            if (isConfirmed) {
+              user.password = req.body.confirmPassword
+              user.resetPasswordToken = undefined
+              user.resetPasswordExpires = undefined
+              return userService.upsertUser(user)
+            } else {
+              const err = new Error('Invalid confirmation password. Please validate your new password.')
+              err.status = HttpStatus.BAD_REQUEST
+              throw err
+            }
+          })
+      })
+      .then(user => {
+        const email = emails.resetPassword().getTemplate()
+        const emailInfo = emailFactory(user.email, email.subject, email.html).getInfo()
+
+        return emailService(emailInfo).send()
+      })
+      .then(data => res.status(HttpStatus.OK).json({ data }))
+      .catch(next)
 
   return {
     auth,
     register,
     newPassword,
-    resetPassword}
+    resetPassword
+  }
 }
