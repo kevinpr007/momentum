47dd5dced27e5532b78f5319677c8a9ddc9c0f62
Kevin Rivera <kevinpr_007@hotmail.com>
2017-08-30 20:38:07 -0400

    Use the Util Helpers file to throw the error and fix tests

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -1,13 +1,13 @@
 const HttpStatus = require('http-status-codes')
+const util = require('../util/util.helpers')
 const _ = require('lodash')
 
 module.exports = roles => (req, res, next) => {
   if (_.intersectionWith(roles, req.user.roles, (role, userRole) => {
     return role.name === userRole.name
   }).length === 0) {
-    const err = new Error('Your user does not have the required role(s) to execute this action.')
-    err.status = HttpStatus.UNAUTHORIZED
-    throw err
+    let message = 'Your user does not have the required role(s) to execute this action.'
+    return util.generateError(message, HttpStatus.UNAUTHORIZED)
   }
   next()
 }
--- src/middleware/page-validations.js
+++ src/middleware/page-validations.js
@@ -1,20 +1,19 @@
 const HttpStatus = require('http-status-codes')
 const config = require('../config/config')
+const util = require('../util/util.helpers')
 
 module.exports = (req, res, next) => {
   req.query.page = parseInt(req.query.page || 0)
   req.query.pageSize = parseInt(req.query.pageSize || config.PAGE_SIZE)
 
   if (req.query.page === undefined || isNaN(req.query.page)) {
-    const err = new Error('You must provide a page number')
-    err.status = HttpStatus.BAD_REQUEST
-    next(err)
+    let message = 'You must provide a page number'
+    return util.generateError(message, HttpStatus.BAD_REQUEST)
   }
 
   if (req.query.pageSize === undefined || isNaN(req.query.pageSize)) {
-    const err = new Error('Page size must be a number')
-    err.status = HttpStatus.BAD_REQUEST
-    next(err)
+    let message = 'Page size must be a number'
+    return util.generateError(message, HttpStatus.BAD_REQUEST)
   }
 
   next()
